1b989e465ce81080fd256406a0a98622bb844974
JustSimon01 <simon.ch@yandex.ru>
2023-04-04 15:53:50 +0300

    queue

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from "../../utils/hooks";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import styles from "./queue-page.module.css"
 import { Queue } from "./queue/queue";
@@ -8,20 +9,88 @@ import { Circle } from "../ui/circle/circle";
 import { sleep } from "../../utils/functions";
 import { ElementStates } from "../../types/element-states";
 
-export const QueuePage: React.FC = () => {
+const queue = new Queue<number>(7);
 
+export const QueuePage: React.FC = () => {
+  interface IqueueArray {
+    array: (number | undefined)[],
+    head: number,
+    tail: number,
+    modifiedItem: number | null
+  }
+
+  const queueLength = queue.getLength();
+  const [queueArray, setQueueArray] = useState<IqueueArray>({
+    array: queue.getQueue(),
+    head: queue.getHead(),
+    tail: queue.getTail(),
+    modifiedItem: null
+  });
+  const {values, handleChange, setValues} = useForm(
+    {
+      item: "",
+    });
+  const [controls, setControls] = useState({
+    add: false,
+    delete: false,
+    clear: false
+  })
+  
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if(values.item) {
+
+      setQueueArray({...queueArray, modifiedItem: queue.getTail()})
+      setControls({...controls, add: true, delete: true, clear: true})
+
+      await sleep(500)
+
+      queue.enqueue(values.item)
+      setQueueArray({...queueArray, array: queue.getQueue(), head: queue.getHead(), tail: queue.getTail() , modifiedItem: null})
+      setControls({...controls, add: false, delete: false, clear: false})
+      setValues({item: ''})
+    }
+  }
+
+  async function dequeue() {
+
+    setQueueArray({...queueArray, modifiedItem: queue.getHead()})
+    setControls({...controls, add: true, delete: true, clear: true})
+
+    await sleep(500)
+
+    queue.dequeue()
+    setQueueArray({...queueArray, array: queue.getQueue(), head: queue.getHead(), tail: queue.getTail() , modifiedItem: null})
+    setControls({...controls, add: false, delete: false, clear: false})
+  }
+
+  function clear() {
+    queue.clear();
+    setQueueArray({...queueArray, array: queue.getQueue(), head: queue.getHead(), tail: queue.getTail()})
+  }
+  
   return (
     <SolutionLayout title="Очередь">
-      <form className={styles.controls} >
+      <form className={styles.controls} onSubmit={onSubmit}>
         <div className={styles.controlsMain}>
-          <Input isLimitText={true}  maxLength={4} />
-          <Button type={'submit'} extraClass={styles.smallButton} text="Добавить"/>
-          <Button type={'button'} extraClass={styles.smallButton} text="Удалить"/>
+          <Input isLimitText={true} value={values.item} name={'item'} maxLength={4} onChange={handleChange}/>
+          <Button type={'submit'} extraClass={styles.smallButton} text="Добавить" disabled={controls.add}/>
+          <Button type={'button'} extraClass={styles.smallButton} text="Удалить" disabled={queueLength === 0 || controls.delete} onClick={dequeue}/>
         </div>
-        <Button type={'reset'} extraClass={styles.smallButton} text="Очистить"/>
+        <Button type={'reset'} extraClass={styles.smallButton} text="Очистить" disabled={queueArray.tail  === 0 || controls.clear}  onClick={clear}/>
       </form>
       <div className={styles.array}>
-      <Circle letter={'1'} key={'index'} />
+        {queueArray.array.map((item, index) => 
+          <Circle 
+            letter={item?.toString()}
+            key={index}
+            index ={index}
+            state={
+              queueArray.modifiedItem === index ? ElementStates.Changing : ElementStates.Default}
+            head={queueArray.array[index] && index === queueArray.head ? "head" : null}
+            tail={queueArray.array[index] && index+1 === queueArray.tail ? "tail" : null}
+          />
+        )}
       </div>
     </SolutionLayout>
   );
--- src/components/queue-page/queue/queue.ts
+++ src/components/queue-page/queue/queue.ts
@@ -1,11 +1,11 @@
 interface IQueue<T> {
   enqueue: (item: T) => void;
   dequeue: () => void;
-  peak: () => T | null;
+  peak: () => T | undefined;
 }
 
 export class Queue<T> implements IQueue<T> {
-  private container: (T | null)[] = [];
+  private container: (T | undefined)[] = [];
   private head = 0;
   private tail = 0;
   private readonly size: number = 0;
@@ -20,27 +20,45 @@ export class Queue<T> implements IQueue<T> {
     if (this.length >= this.size) {
       throw new Error("Maximum length exceeded");
     }
+    if (this.tail >= this.size) {
+      throw new Error("Maximum length exceeded");
+    }
     this.container[this.tail % this.size] = item;
     this.tail++;
     this.length++;
-
   };
 
   dequeue = () => {
     if (this.isEmpty()) {
       throw new Error("No elements in the queue");
     }
-    this.container[this.head] = null;
+    this.container[this.head] = undefined;
     this.head++;
     this.length--;
   };
 
-  peak = (): T | null => {
+  peak = (): T | undefined => {
     if (this.isEmpty()) {
       throw new Error("No elements in the queue");
     }
    return this.container[this.head % this.size];
   };
 
+  clear = () => {
+    this.container = Array(this.size);
+    this.head = 0;
+    this.tail = 0;
+    this.length = 0;
+  }
+
   isEmpty = () => this.length === 0;
+
+  getSize = () => this.size;
+
+  getQueue = () => [...this.container];
+
+  getLength = () => this.length
+  getTail = () => this.tail
+  getHead = () => this.head
+
 }
